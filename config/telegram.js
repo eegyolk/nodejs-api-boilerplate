ae026646c250e3403a9aff3bcafcda786459eb96
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-02-22 21:14:03 +0800

    Removed validation of env var

--- config/telegram.js
+++ config/telegram.js
@@ -1,14 +1,5 @@
-const EventEmitter = require('events');
-
 module.exports = {
   baseUrl: process.env.TELEGRAM_BASE_URL || 'https://api.telegram.org/',
-  botToken:
-    process.env.TELEGRAM_BOT_TOKEN ||
-    new EventEmitter().emit(
-      'error',
-      new Error('TELEGRAM_BOT_TOKEN is missing!')
-    ),
-  chatId:
-    process.env.TELEGRAM_CHAT_ID ||
-    new EventEmitter().emit('error', new Error('TELEGRAM_CHAT_ID is missing!')),
+  botToken: process.env.TELEGRAM_BOT_TOKEN || null,
+  chatId: process.env.TELEGRAM_CHAT_ID || null,
 };
