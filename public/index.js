53a6b649f1574bd8159ce81641b34c443a33d773
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-02-22 19:54:56 +0800

    Initial logic of the app entrypoint file

---
+++ public/index.js
@@ -0,0 +1,23 @@
+const express = require('express'),
+  app = express(),
+  compression = require('compression');
+
+const appBootstrap = require('../bootstrap/app'),
+  config = appBootstrap.config;
+
+app.use(compression({ threshold: 0 }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.disable('x-powered-by');
+
+appBootstrap.extendApp({ app });
+
+if (config.app.env !== 'test') {
+  app.listen(config.app.port, () => {
+    console.log(
+      `Server listening at port ${config.app.port} with process id ${process.pid}`
+    );
+  });
+} else {
+  module.exports = app;
+}
