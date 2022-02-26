6e74d0aed93d519cb26a76650a0b91d50897db47
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-02-26 17:32:18 +0800

    Create UsersService class file with initial function createUser

---
+++ app/Services/UsersService.js
@@ -0,0 +1,39 @@
+const BaseService = require('./BaseService');
+const HttpCodeConstant = require('../Constants/HttpCodeConstant');
+const SuccessResponse = require('../Response/SuccessResponse');
+const UsersRepository = require('../Repositories/Lab15Soln/UsersRepository');
+const Validation = require('../Classes/Validation');
+
+class UsersService extends BaseService {
+  static linkType = 'users';
+
+  static async createUser(body, databaseConnections, url) {
+    const rules = {
+      name: 'required',
+      email: 'required|email',
+      password: 'required',
+    };
+
+    const validation = new Validation(body, rules);
+    validation.validate();
+
+    const attributes = {
+      name: body.name,
+      email: body.email,
+      password: body.password,
+    };
+
+    const data = await UsersRepository.createUser(
+      attributes,
+      databaseConnections.mysql
+    );
+
+    return new SuccessResponse(
+      HttpCodeConstant.CREATED,
+      this.resolveLinks(url, this.linkType, data.id),
+      data
+    );
+  }
+}
+
+module.exports = UsersService;
