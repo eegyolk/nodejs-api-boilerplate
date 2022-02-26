26645dbd9d5a18a39767def51c4d08e795a7afa6
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-02-26 17:30:19 +0800

    Created SuccessResponse class

---
+++ app/Response/SuccessResponse.js
@@ -0,0 +1,29 @@
+class SuccessResponse {
+  constructor(httpCode, links, data, relationships = null, meta = null) {
+    this.httpCode = httpCode;
+    this.links = links;
+    this.data = data;
+    this.relationships = relationships;
+    this.meta = meta;
+  }
+
+  getHttpCode() {
+    return this.httpCode;
+  }
+
+  getContent() {
+    const content = { links: this.links, data: this.data };
+
+    if (this.relationships) {
+      content['relationships'] = this.relationships;
+    }
+
+    if (this.meta) {
+      content['meta'] = this.meta;
+    }
+
+    return content;
+  }
+}
+
+module.exports = SuccessResponse;
