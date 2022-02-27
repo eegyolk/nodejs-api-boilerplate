7faad4a32ba7af7a67af8ecb6a275a6782a57af8
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-02-27 23:52:38 +0800

    Implemented CheckHeaderMiddleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,15 @@
+const CheckHeaderMiddleware = require('../../app/Http/Middleware/CheckHeaderMiddleware');
 const UsersController = require('../../app/Http/Controllers/UsersController');
 
 module.exports.setRoutes = function (router) {
-  router.post('/users', UsersController.createUser);
+  router.post(
+    '/users',
+    [CheckHeaderMiddleware.accept, CheckHeaderMiddleware.contentType],
+    UsersController.createUser
+  );
+  router.get(
+    '/users/:id',
+    [CheckHeaderMiddleware.accept],
+    UsersController.getUser
+  );
 };
