0e55f2bb12a1a5cb9ac096ff74a0624f3ebf9687
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-02-28 12:10:45 +0800

    Throw exception for lesser code, BaseMiddleware will handle it

--- app/Http/Middleware/CheckHeaderMiddleware.js
+++ app/Http/Middleware/CheckHeaderMiddleware.js
@@ -1,8 +1,43 @@
-class CheckHeaderMiddleware {
-  static execute(req, res) {
+const BadRequestException = require('../../Exceptions/BadRequestException');
+const BaseMiddleware = require('./BaseMiddleware');
+const HttpCodeConstant = require('../../Constants/HttpCodeConstant');
+const UnsupportedMediaTypeException = require('../../Exceptions/UnsupportedMediaTypeException');
+
+class CheckHeaderMiddleware extends BaseMiddleware {
+  static accept(req, res, next) {
     const { headers } = req;
 
-    next();
+    try {
+      if (typeof headers['accept'] !== 'undefined') {
+        if (headers['accept'] === 'application/vnd.api+json') {
+          next();
+          return;
+        }
+      }
+
+      throw new BadRequestException(HttpCodeConstant.BAD_REQUEST);
+    } catch (err) {
+      BaseMiddleware.errorHandler(res, err);
+    }
+  }
+
+  static contentType(req, res, next) {
+    const { headers } = req;
+
+    try {
+      if (typeof headers['content-type'] !== 'undefined') {
+        if (headers['content-type'] === 'application/vnd.api+json') {
+          next();
+          return;
+        }
+      }
+
+      throw new UnsupportedMediaTypeException(
+        HttpCodeConstant.UNSUPPORTED_MEDIA_TYPE
+      );
+    } catch (err) {
+      BaseMiddleware.errorHandler(res, err);
+    }
   }
 }
 
