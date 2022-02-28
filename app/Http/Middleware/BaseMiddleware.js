2e42611b87392e9122ccc530fab733821d3ae561
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-02-28 12:10:10 +0800

    Created BaseMiddleware, initial function errorHandler

---
+++ app/Http/Middleware/BaseMiddleware.js
@@ -0,0 +1,47 @@
+const BadRequestException = require('../../Exceptions/BadRequestException');
+const ErrorResponse = require('../../Response/ErrorResponse');
+const HttpCodeConstant = require('../../Constants/HttpCodeConstant');
+const UnknownException = require('../../Exceptions/UnknownException');
+const UnsupportedMediaTypeException = require('../../Exceptions/UnsupportedMediaTypeException');
+
+class BaseMiddleware {
+  static errorHandler(res, err) {
+    if (err instanceof BadRequestException) {
+      const response = new ErrorResponse(
+        err.getStatus(),
+        err.getCode(),
+        err.getTitle(),
+        err.getErrors()
+      );
+
+      res.set('Content-Type', 'application/vnd.api+json');
+      res.status(response.getHttpCode()).json(response.getContent());
+      return;
+    }
+
+    if (err instanceof UnsupportedMediaTypeException) {
+      const response = new ErrorResponse(
+        err.getStatus(),
+        err.getCode(),
+        err.getTitle(),
+        err.getErrors()
+      );
+
+      res.set('Content-Type', 'application/vnd.api+json');
+      res.status(response.getHttpCode()).json(response.getContent());
+      return;
+    }
+
+    const response = new ErrorResponse(
+      HttpCodeConstant.INTERNAL_SERVER_ERROR,
+      UnknownException.getCode(),
+      UnknownException.getTitle(),
+      UnknownException.getErrors()
+    );
+
+    res.set('Content-Type', 'application/vnd.api+json');
+    res.status(response.getHttpCode()).json(response.getContent());
+  }
+}
+
+module.exports = BaseMiddleware;
